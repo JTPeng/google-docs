fc6540d968dd9bc5c6d9fca96054975e31d9c817
JTPeng <404502753@qq.com>
2024-12-23 18:14:36 +0800

    feat: add tiptap plugin and A4 paper

--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,9 +1,11 @@
-interface DocumentsIdPageProps {
-  params: Promise<{ documentId: string }>;
-}
-const DocumentsIdPage = async ({ params }: DocumentsIdPageProps) => {
-  const { documentId } = await params;
-  return <div>Document ID: {documentId}</div>;
+import Editor from './editor';
+
+const DocumentsIdPage = async () => {
+  return (
+    <div className="min-h-screen bg-[#f9fbfd]">
+      <Editor />
+    </div>
+  );
 };
 
 export default DocumentsIdPage;
--- src/app/documents/layout.tsx
+++ src/app/documents/layout.tsx
@@ -4,7 +4,6 @@ interface DocumentLayoutProps {
 const DocumentLayout = ({ children }: DocumentLayoutProps) => {
   return (
     <div className="flex flex-col gap-y-4">
-      <nav className="w-full bg-red-500">navBar</nav>
       {children}
     </div>
   );
