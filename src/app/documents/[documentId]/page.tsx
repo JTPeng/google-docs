5c684a01e3670d46fa57ab758ee3a26a20418580
JTPeng <404502753@qq.com>
2024-12-23 15:23:26 +0800

    feat: layout 布局的作用

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,9 @@
+interface DocumentsIdPageProps {
+  params: Promise<{ documentId: string }>;
+}
+const DocumentsIdPage = async ({ params }: DocumentsIdPageProps) => {
+  const { documentId } = await params;
+  return <div>Document ID: {documentId}</div>;
+};
+
+export default DocumentsIdPage;
---
+++ src/app/documents/settings/page.tsx
@@ -0,0 +1,9 @@
+const DocumentSettingsPage = () => {
+  return (
+    <div>
+      <h1>Document Settings</h1>
+    </div>
+  );
+};
+
+export default DocumentSettingsPage;
