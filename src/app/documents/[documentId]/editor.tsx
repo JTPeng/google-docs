ab6113cd21bdd96fbdc5c9db8f0fda001c7bbd5b
JTPeng <404502753@qq.com>
2024-12-31 11:26:52 +0800

    feat: link button and image button

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -13,6 +13,7 @@ import ImageResize from 'tiptap-extension-resize-image';
 import Underline from '@tiptap/extension-underline';
 import FontFamily from '@tiptap/extension-font-family';
 import TextStyle from '@tiptap/extension-text-style';
+import Link from '@tiptap/extension-link'
 import { useEditor, EditorContent } from '@tiptap/react';
 import { useEditorStore } from '@/store/use-editor-store';
 
@@ -70,6 +71,11 @@ const Editor = () => {
       Color,
       Highlight.configure({
         multicolor: true,
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: 'https:',
       }),
     ],
     content: `
